62760bf119e8bcdf2c6c13c4c3874143f38b2ded
Red7rocks <burnsaj@vcu.edu>
2021-03-11 16:07:01 -0500

    open notepad.exe

---
+++ openNote.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+int main(void){
+	system("notepad.exe C:\\cd..\\cd..\\Program Files\\Notepad++\\readme.txt");
+	//system("Start C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+	return 0;
+}
